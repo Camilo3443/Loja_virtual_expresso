8b8a6ad8432f3e163cb4eec063bca45d06f9e55f
Rodrigo <rodrigo.rampazzo70@gmail.com>
2019-01-22 10:18:11 -0200

    teste

--- app/src/test/java/br/com/rcc/lojavirtual/ExampleUnitTest.java
+++ app/src/test/java/br/com/rcc/lojavirtual/ExampleUnitTest.java
@@ -8,8 +8,6 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(, 2 + 2);
-    }
+
+
 }
